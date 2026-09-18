4853653c8a43b78d13e4532919ea1cd9c1271dbf
nullps1 <noah@weeksitsolutions.com>
2026-09-18 16:45:47 -0600

    Scale copper equipment durability wear with oxidation before Unbreaking

--- src/main/java/com/nullps1/copperworks/item/CopperArmorItem.java
+++ src/main/java/com/nullps1/copperworks/item/CopperArmorItem.java
@@ -2,17 +2,26 @@ package com.nullps1.copperworks.item;
 
 import com.nullps1.copperworks.Copperworks;
 import com.nullps1.copperworks.data.CopperEquipment;
+import java.util.function.Consumer;
+import javax.annotation.Nullable;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public final class CopperArmorItem extends ArmorItem {
     public CopperArmorItem(Holder<ArmorMaterial> material, Type type, Properties properties) {
         super(material, type, properties);
+    }
+
+    @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, @Nullable T entity, Consumer<Item> onBroken) {
+        return CopperEquipment.scaleDurabilityDamage(stack, amount, entity);
     }
 
     @Override
